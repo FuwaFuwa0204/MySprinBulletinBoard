e2ee678d32c5a4961d1e8db31cb28200628895cd
FuwaFuwa0204 <soyeonmail24@gmail.com>
2024-03-29 21:21:47 +0900

    add question thymeleaf templates, QuestionService

---
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,38 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/question")
+public class QuestionController {
+	
+	private final QuestionService questionService;
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		//List<Question> questionList = this.questionRepository.findAll();
+		List<Question> questionList = this.questionService.getList();
+		//name,value
+		model.addAttribute("questionList",questionList);
+		//이제 이 model 객체를 템플릿에서 활용한다.
+		//파일명
+		return "question_list";
+	}
+	
+	@GetMapping(value= "/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question",question);
+		return "question_detail";
+	}
+
+}
