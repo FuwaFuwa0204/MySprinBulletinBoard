fac5d07b52fa2d489a79a9114350837ab91d405e
FuwaFuwa0204 <soyeonmail24@gmail.com>
2024-04-05 21:24:20 +0900

    add SiteUserController, SiteUserService, SiteUserForm, signup_form.html

--- sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
+++ sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb;
 
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,16 +9,27 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//스프링의 환경설정 파일
 @Configuration
+//모든 요청 url이 스프링 시큐리티의 제어를 받도록
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
+	//이 클래스로 모든 요청 url에 필터 작용을 시켜 url별로 특별 설정을 할 수 있게 된다.
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		http.authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests.requestMatchers(new AntPathRequestMatcher("/**"))
 				.permitAll())
 		.csrf((csrf)->csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
 		.headers((headers)->headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
 		return http.build();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
