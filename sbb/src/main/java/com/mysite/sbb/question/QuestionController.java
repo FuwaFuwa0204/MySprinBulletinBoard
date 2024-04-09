303f7438b4e6bbcfc9e8ccc788b87e5dfdcc76c0
FuwaFuwa0204 <soyeonmail24@gmail.com>
2024-04-09 21:37:20 +0900

    feat modify btn

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,10 +1,5 @@
 package com.mysite.sbb.question;
 
-import java.util.List;
-
-
-
-
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +23,8 @@ import org.springframework.data.domain.Page;
 import java.security.Principal;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
 @Controller
@@ -74,6 +71,37 @@ public class QuestionController {
 		}
 	    this.questionService.create(questionForm.getSubject(),questionForm.getContent(), siteUser);
 		return "redirect:/question/list";
+	}
+	
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/modify/{id}")
+	public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal, @PathVariable("id") Integer id) {
+		
+		if(bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		Question question = this.questionService.getQuestion(id);
+		
+		if(!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+		}
+		this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+		return String.format("redirect:/question/detail/%s",id);
+	}
+
+	
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/modify/{id}")
+	public String questionModify(QuestionForm questionForm, Principal principal, @PathVariable("id") Integer id) {
+
+		Question question = this.questionService.getQuestion(id);
+		
+		if(!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+		}
+		questionForm.setSubject(question.getSubject());
+		questionForm.setContent(question.getContent());
+		return "question_form";
 	}
 
 }
